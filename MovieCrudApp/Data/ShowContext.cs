fa49b3d097a3ca373930f958cfc8d5b045ca990c
Atlas Sandevi <62032234+DigitalDisco@users.noreply.github.com>
2023-02-18 16:14:11 +0100

    Add files via upload

---
+++ MovieCrudApp/Data/ShowContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieCrudApp.Models;
+
+namespace MovieCrudApp.Data
+{
+    public class ShowContext : DbContext
+    {
+        public DbSet<Show> Shows { get; set; }
+        public ShowContext(DbContextOptions<ShowContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ MovieCrudApp/Models/Show.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MovieCrudApp.Data;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace MovieCrudApp.Models
+{
+    public class Show
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public int Interest { get; set; }
+        public double? Rating { get; set; }
+        public string ImgUrl { get; set; }
+
+        public async Task GetTVMazeData()
+        {
+            using (var client = new HttpClient())
+            {
+                UriBuilder builder = new UriBuilder("https://api.tvmaze.com/singlesearch/shows");
+                builder.Query = $"q={this.Name}";
+                HttpResponseMessage resp = await client.GetAsync(builder.Uri);
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    var result = await resp.Content.ReadAsStringAsync();
+
+                    dynamic respData = JObject.Parse(result);
+                    this.Rating = respData?.rating?.average ?? 0;
+
+                    if (respData?.image?.medium != null)
+                    {
+                        this.ImgUrl = respData?.image?.medium ?? "";
+                    }
+                    else
+                    {
+                        this.ImgUrl = "https://via.placeholder.com/210";
+                    }
+
+                    if (respData.genres.Count > 0)
+                    {
+                        this.Genre = respData.genres.First;
+                    }
+                    else
+                    {
+                        this.Genre = respData.type;
+                    }
+
+                }
+                else
+                {
+                    this.ImgUrl = "https://via.placeholder.com/210";
+                }
+            }
+        }
+    }
+
+
+}
